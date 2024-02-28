627a7fb4e12e3ca3e573a34fad9d001af7ba5a2f
Mohd Ramzan Shareef <107868540+itisRamzan@users.noreply.github.com>
2024-02-28 10:37:23 +0530

    test-done

--- src/app/(chat-routes)/chats/page.js
+++ src/app/(chat-routes)/chats/page.js
@@ -1,6 +1,6 @@
 import Homepage from "@/ui/chat/HomePage";
 
-export default function ChatsHomePage() {
+export default async function ChatsHomePage() {
     return (
         <>
             <Homepage />
